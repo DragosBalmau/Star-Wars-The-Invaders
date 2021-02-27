afec7324816fea11ab7d168f3a37896400304c9f
DragosBalmau <balmaudragos@gmail.com>
2021-02-28 00:18:10 +0200

    Structure updare

--- Star Wars The Invaders/constants.py
+++ Star Wars The Invaders/constants.py
@@ -8,23 +8,26 @@ color_dark = (100, 100, 100)
 color_blue = (0, 102, 255)
 
 # Images
-player_img = "Resources/x-wing.png"
-enemy_img = "Resources/fighter.png"
+player_img = "Resources/Textures/x-wing.png"
+enemy_img = "Resources/Textures/fighter.png"
 
-laser_img_blue = "Resources/laser_bullet_blue.png"
-laser_img_red = "Resources/laser_bullet_red.png"
+laser_img_blue = "Resources/Textures/laser_bullet_blue.png"
+laser_img_red = "Resources/Textures/laser_bullet_red.png"
 
-logo_star_wars_img = "Resources/star_wars_logo.png"
-logo_python = "Resources/python_logo.png"
-logo_pygame = "Resources/pygame_logo.png"
+logo_star_wars_img = "Resources/Textures/star_wars_logo.png"
+logo_python = "Resources/Textures/python_logo.png"
+logo_pygame = "Resources/Textures/pygame_logo.png"
 
-background_menu = "Resources/background_menu.png"
-background_game = "Resources/background_game.png"
+background_menu = "Resources/Textures/background_menu.png"
+background_game = "Resources/Textures/background_game.png"
 
-republican = "Resources/republican_pilot.png"
-empire = "Resources/empire_pilot.png"
+republican = "Resources/Textures/republican_pilot.png"
+empire = "Resources/Textures/empire_pilot.png"
+
+win = "Resources/Textures/you_won.png"
+lose = "Resources/Textures/game_over.png"
 
 # Sounds
-theme_star_wars = "Resources/Star-Wars-Intro.mp3"
-fighter_sound = "Resources/fighter_sound.mp3"
-xwing_sound = "Resources/x-wing_sound.mp3"
+theme_star_wars = "Resources/Sounds/Star-Wars-Intro.mp3"
+fighter_sound = "Resources/Sounds/fighter_sound.mp3"
+xwing_sound = "Resources/Sounds/x-wing_sound.mp3"
